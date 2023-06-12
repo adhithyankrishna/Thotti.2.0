7359b4f12c6bbb655f5d73dd2af661ff9c947667
adhithyan Krishna <krishnaadhithyan@gmail.com>
2023-06-12 11:56:20 +0530

    commit 21

--- src/Chat.js
+++ src/Chat.js
@@ -3,15 +3,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 import 'firebase/compat/storage';
-//import { LineProgressBar } from '@frogress/line';
 
 const Chat = ({ firestore }) => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
   const [file, setFile] = useState(null);
   const [filename, setFilename] = useState(null);
- // const [progress, setProgress] = useState(0);
- // const [showProgressBar, setShowProgressBar] = useState(false);
   const chatContainerRef = useRef(null);
   const inputContainerRef = useRef(null);
   
@@ -32,9 +29,6 @@ const Chat = ({ firestore }) => {
   }, [firestore]);
 
   const handleSendMessage = async () => {
-    // Check if the message is empty
-    // if (newMessage.trim() === '') return;
-  
     const messageData = {
       text: newMessage,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
@@ -47,39 +41,24 @@ const Chat = ({ firestore }) => {
   
       const uploadTask = fileRef.put(file);
   
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-          // Calculate the progress percentage
-          const progress = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          );
+      uploadTask.then(() => {
+        // When the upload is complete, get the file download URL
+        uploadTask.snapshot.ref.getDownloadURL().then((fileUrl) => {
+          messageData.file = fileUrl;
+          messageData.filename = filename;
   
-          // Update the progress state
-        //  setProgress(progress);
-         // setShowProgressBar(true);
-        },
-        (error) => {
-          // Handle any errors that occur during the upload
-          alert('Something bad happened');
-        },
-        () => {
-          // When the upload is complete, get the file download URL
-          uploadTask.snapshot.ref.getDownloadURL().then((fileUrl) => {
-            messageData.file = fileUrl;
-            messageData.filename = filename;
-  
-            // Add the message to Firestore
-            firestore.collection('chat').add(messageData).then(() => {
-              // Reset the input values
-              setNewMessage('');
-              setFile(null);
-              setShowProgressBar(false);
-              setProgress(0);
-            });
+          // Add the message to Firestore
+          firestore.collection('chat').add(messageData).then(() => {
+            // Reset the input values
+            setNewMessage('');
+            setFile(null);
           });
-        }
-      );
+        });
+      }).catch((error) => {
+        // Handle any errors that occur during the upload
+        console.error('Error uploading file:', error);
+        alert('Something bad happened');
+      });
     } else {
       // If there is no file, simply add the message to Firestore
       await firestore.collection('chat').add(messageData);
@@ -101,9 +80,7 @@ const Chat = ({ firestore }) => {
   };
   
   const handleKeyPress = (e) => {
-    
     if (e.key === 'Enter') {
-      
       handleSendMessage();
     }
   };
@@ -117,42 +94,33 @@ const Chat = ({ firestore }) => {
   return (
     <div className="chat-container">
       <div className="scroll-button-container">
-        <button onClick={scrollToBottom}  class='scroll' >Scroll to End</button>
+        <button onClick={scrollToBottom} className="scroll">Scroll to End</button>
       </div>
       <div className="messages-container" ref={chatContainerRef}>
         {messages.map((message, index) => (
           <div className="message" key={index}>
-
             <p>{message.text}</p>
-            <div class='fileli'>
-            {message.file && (
-              <a href={message.file} download>
-                {message.filename}
-              </a>
-            )}
+            <div className="fileli">
+              {message.file && (
+                <a href={message.file} download>
+                  {message.filename}
+                </a>
+              )}
             </div>
           </div>
         ))}
-      </div >
-  
-      {showProgressBar && (
-        <div className='prog'>
-          <LineProgressBar percent={progress} />
-        </div>
-      )}
-      <div className="input-container"  ref={inputContainerRef}>
-        <input placeholder='Enter the message'
+      </div>
+      <div className="input-container" ref={inputContainerRef}>
+        <input
+          placeholder="Enter the message"
           type="text"
           value={newMessage}
           onKeyDown={handleKeyPress}
-          onChange={(e) => setNewMessage(e.target.value) }
+          onChange={(e) => setNewMessage(e.target.value)}
         />
         <input type="file" onChange={handleFileChange} onKeyDown={handleKeyPress} />
-        
-        
         <button onClick={handleSendMessage}>Send</button>
       </div>
-      
     </div>
   );
 };
